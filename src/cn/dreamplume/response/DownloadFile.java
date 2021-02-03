a3a4e0e54193455a09dc94ad5c359cdaea44a4a6
Wmt-Monica <3040988158@qq.com>
2021-02-03 10:27:50 +0800

    2021/2/3 使用 servlet 在网页上下载各类型的文件 版本更改，解决中文和针对各种浏览器来进行修改

--- src/cn/dreamplume/response/DownloadFile.java
+++ src/cn/dreamplume/response/DownloadFile.java
@@ -1,5 +1,7 @@
 package cn.dreamplume.response;
 
+import sun.misc.BASE64Encoder;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
 
 /**
  * @Classname ${NAME}
@@ -31,6 +34,27 @@ public class DownloadFile extends HttpServlet {
 
         // 获取下载的文件的文件名  设置a标签中 href 的文件参数 filename 值
         String  filename = request.getParameter("filename");
+
+        // 避免中文乱码将字符通过更改字节编码成通用的 UTF-8 编码形式
+        filename = new String(filename.getBytes("ISO8859-1"),"UTF-8");
+
+        // 获取浏览器请求头中 User_Agent 字符串
+        String agent = request.getHeader("User-Agent");
+
+        // 根据浏览器的 User_Agent 的字符串内容判断执行浏览器的类型
+        if (agent.contains("MSIE")) {
+            // IE浏览器
+            filename = URLEncoder.encode(filename, "utf-8");
+            filename = filename.replace("+", " ");
+        } else if (agent.contains("Firefox")) {
+            // 火狐浏览器
+            BASE64Encoder base64Encoder = new BASE64Encoder();
+            filename = "=?utf-8?B?"
+                    + base64Encoder.encode(filename.getBytes("utf-8")) + "?=";
+        } else {
+            // 其它浏览器
+            filename = URLEncoder.encode(filename, "utf-8");
+        }
 
         // 获取要下载文件的文件了类型-----客户端通过文件的 MIME 类型去区分类型
         response.setContentType(this.getServletContext().getMimeType(filename));
