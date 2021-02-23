91c664ccc3cce489f47c89109977942d604c3453
Wmt-Monica <3040988158@qq.com>
2021-02-23 23:56:46 +0800

    2021/2/23

---
+++ src/cn/dreamplume/ajax/AjaxServlet.java
@@ -0,0 +1,29 @@
+package cn.dreamplume.ajax;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Classname ${NAME}
+ * @Description TODO
+ * @Date 2021/2/23 23:36
+ * @Created by 翊
+ */
+@WebServlet(name = "AjaxServlet")
+public class AjaxServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("ajax相应传回的信息");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
